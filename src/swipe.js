b8bf6c589f673adf77a2a864f8ce5f50e7d92035
Bearbearsbears <143830063+Bearbearsbears@users.noreply.github.com>
2025-11-02 20:40:16 -0600

    Update swipe.js

--- src/swipe.js
+++ src/swipe.js
@@ -1,3 +1,7 @@
+startx = 0;
+endx= 0;
+//observes whether or not the user has touched screen and/or finished touching screen
+//horizontal axis
 touch.addEventListener('start', function (event) {
   startx = e.changedTouches[0].screenX;
 })
@@ -6,6 +10,8 @@ touch.addEventListener('end', function (event){
   checkDirection();
 })
 function checkDirection(){
+  //compare horizontal distance to determine if user swiped left or right
+  // need to adhere this to like/dislike buttons
   if (startx > endx){
     console.log ('swiped left')}
   if (startx <endx){
