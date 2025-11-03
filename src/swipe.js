b6741109e17173c4f13ce8f463ea06b10f36a305
Trey McDonald <tmcdo26@lsu.edu>
2025-11-02 21:56:09 -0600

    Create swipe.js

---
+++ src/swipe.js
@@ -0,0 +1,45 @@
+import { useRef } from "react"
+
+export default function useSwipe(onSwipeLeft, onSwipeRight) {
+  const startX = useRef(0)
+  const endX = useRef(0)
+//observes whether or not the user has touched screen and/or finished touching screen
+//horizontal axis
+// Touch swipe for mobile
+  const handleTouchStart = (e) => {
+    startX.current = e.touches[0].clientX
+  }
+
+  const handleTouchEnd = (e) => {
+    endX.current = e.changedTouches[0].clientX
+    checkDirection()
+  }
+
+  // Mouse swipe for desktop
+  const handleMouseDown = (e) => {
+    startX.current = e.clientX
+  }
+
+  const handleMouseUp = (e) => {
+    endX.current = e.clientX
+    checkDirection()
+  }
+const checkDirection = () => {
+  //compare horizontal distance to determine if user swiped left or right
+  // need to adhere this to like/dislike buttons
+ if (startX.current > endX.current + 50) {
+      console.log("Swiped left")
+      onSwipeLeft?.()
+    } else if (startX.current < endX.current - 50) {
+      console.log("Swiped right")
+      onSwipeRight?.()
+    }
+  }
+
+  return {
+    onTouchStart: handleTouchStart,
+    onTouchEnd: handleTouchEnd,
+    onMouseDown: handleMouseDown,
+    onMouseUp: handleMouseUp,
+  }
+}
