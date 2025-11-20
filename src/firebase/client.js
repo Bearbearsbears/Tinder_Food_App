7688a577d138ee579a2e55ee2caf4d3ce1cc7908
joesevv <joseph.severino1@gmail.com>
2025-11-20 08:27:14 -0600

    wrote it in ts in vscode so had to change syntax to js
    Removed type assertions from Firebase config values.

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FB_API_KEY as string,
-  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_FB_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_FB_APP_ID as string,
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID ,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET ,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID ,
 };
 //initializes firebase
 const app = initializeApp(firebaseConfig);
