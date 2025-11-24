035ac92cd6759932d959fc01a34b0ec8f44651f1
Trey McDonald <tmcdo26@lsu.edu>
2025-11-23 21:42:54 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FB_API_KEY as string,
-  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_FB_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_FB_APP_ID as string,
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_AUTH_DOMAIN_HERE",
+  projectId: "YOUR_PROJECT_ID_HERE",
+  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "YOUR_APP_ID_HERE",
 };
-//initializes firebase
+
 const app = initializeApp(firebaseConfig);
-// auth context export
 export const auth = getAuth(app);
-//exporting login w google provider
 export const googleProvider = new GoogleAuthProvider();
-// firestore for saving likes/dislikes
 export const db = getFirestore(app);
