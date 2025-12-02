f386988c0dddad77ee9610cec66cb428cd170fed
Trey McDonald <tmcdo26@lsu.edu>
2025-12-02 11:35:20 -0600

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -118,7 +118,7 @@ export default function Login() {
               fontWeight: 900,
             }}
           >
-            Eater&apos;s Hack
+            BiteSwipe
           </h1>
           <p
             style={{
