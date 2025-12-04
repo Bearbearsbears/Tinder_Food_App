adc04ae30e4569c387626fe1f67fc2b495401fd6
joesevv <joseph.severino1@gmail.com>
2025-12-04 11:05:07 -0600

    use env var

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMMaHfblXxUhz7LJpi2LEREJnDCqFxSx0",
-  authDomain: "foodtinder-4bb14.firebaseapp.com",
-  projectId: "foodtinder-4bb14",
-  storageBucket: "foodtinder-4bb14.firebasestorage.app",
-  messagingSenderId: "593952674549",
-  appId: "1:593952674549:web:07a08254f3db4eefaeabbc",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 //initializes firebase
 const app = initializeApp(firebaseConfig);
