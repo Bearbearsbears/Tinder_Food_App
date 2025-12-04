51183b0772a8f2a7edf34163c21ccab4acce22a2
Ollie Gannon <olliegannon9@gmail.com>
2025-12-03 19:08:31 -0600

    sort

--- src/sort.jsx
+++ src/sort.jsx
@@ -1,22 +1,41 @@
 import React, { useState } from "react";
 //buttons for the filter function-- based upon breakfast, lunch, and dinner. Default 'all' would display all items regardless of filter
-    <div classname='flex justify-content-center'>
+
+export default function Sort() {
+    const[filter, setFilter] = useState("all");
+
+    return (
+      <div className='flex justify-content-center'>
+        
         <button 
-          classname= {'p-1 px-2 mx-4 btn fw-bold ${filter==='all'}'}>
-              All
+          className= {'p-1 px-2 mx-4 btn fw-bold ${filter==="all"}'}
+          onClick={() => setFilter("all")}
+        >
+          All
         </button>
-    <button 
-          classname= {'p-1 px-2 mx-4 btn fw-bold ${filter==='Breakfast'}'}>
-              Breakfast
+
+        <button 
+          className= {'p-1 px-2 mx-4 btn fw-bold ${filter==="Breakfast"}'}
+          onClick={() => setFilter("Breakfast")}
+        >
+          Breakfast
         </button>
-            <button 
-          classname= {'p-1 px-2 mx-4 btn fw-bold ${filter==='Lunch'}'}>
-              Lunch
+
+        <button 
+          className= {'p-1 px-2 mx-4 btn fw-bold ${filter==="Lunch"}'}
+          onClick={() => setFilter("Lunch")}
+        >
+          Lunch
         </button>
-            <button 
-          classname= {'p-1 px-2 mx-4 btn fw-bold ${filter==='Dinner'}'}>
+
+        <button 
+          className= {'p-1 px-2 mx-4 btn fw-bold ${filter==="Dinner"}'}
+          onClick={() => setFilter("Dinner")}
+        >
           Dinner
         </button>
-            
-  </div>
+        
+      </div>
+    );
 }
+
