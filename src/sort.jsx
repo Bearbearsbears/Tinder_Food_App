130474ff352ec7a25f5911e86019209e1bebcc65
Bearbearsbears <143830063+Bearbearsbears@users.noreply.github.com>
2025-12-03 18:00:30 -0600

    Update sort.jsx

--- src/sort.jsx
+++ src/sort.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 //buttons for the filter function-- based upon breakfast, lunch, and dinner. Default 'all' would display all items regardless of filter
     <div classname='flex justify-content-center'>
-    {
-        <button 
-          classname= {'p-1 px-2 mx-4 btn fw-bold'>
-              {val}
-        </button>
-    }
         <button 
           classname= {'p-1 px-2 mx-4 btn fw-bold ${filter==='all'}'}>
               All
